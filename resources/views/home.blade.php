4be035b6a51d0391f3f59ed5bc4281b2c21dd279
Daan de Waard <daan.de.waard@hz.nl>
2020-04-30 16:28:04 +0200

    Remove commented code from home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,40 +27,6 @@
                     </div>
                 </div>
 
-                <!-- <div class="column is-4-desktop is-12-tablet">
-                    <p class="title is-4">Nieuw(s) Online</p>
-
-                    <div class="columns is-multiline">
-
-                        @foreach($latestPosts as $post)
-                            <div class="column is-12">
-                                <div class="card">
-
-                                    <div class="card-image">
-                                        <img src="{{$post->img_url}}" alt="Post picture">
-                                    </div>
-
-                                    <div class="card-content">
-                                        <div class="content">
-
-                                            <a class="title is-4" href="/posts/{{$post->id}}">{{$post->title}}</a>
-
-                                            <p>{{$post->excerpt}}</p>
-                                        </div>
-                                        <div class="has-text-centered">
-                                            <a href="/posts/{{$post->id}}" class="button is-primary">Lees meer...</a>
-                                        </div>
-                                    </div>
-                                    <footer class="card-footer">
-                                        <p class="card-footer-item">Gepubliceerd: {{ $post->published_at }}</p>
-                                    </footer>
-                                </div>
-                            </div>
-                        @endforeach
-
-                    </div>
-
-                </div> -->
             </div>
         </div>
     </section>
