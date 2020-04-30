34e08143a3216b7ab6542de0204a9de819a991cd
Daan de Waard <daan.de.waard@hz.nl>
2020-04-30 20:42:13 +0200

    Update layout of admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,6 +1,32 @@
 @extends('common.page')
 
 @section('content')
-<div class="column is-6-tablet is-5-desktop is-4-widescreen has-text-centered">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium error inventore modi neque nulla porro dolorem provident, molestias vero ea earum asperiores optio et minus eius sint. Distinctio, ut deserunt.</div>
+    <section class="hero  is-medium  is-bold is-primary">
+        <div class="hero-body" style="
+            background: url('/img/20200430_133650.png') no-repeat center bottom;
+            background-size: cover;"
+        ></div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+
+                <div class="column is-offset-3-desktop is-6-desktop is-12-tablet">
+
+                    <div class="content">
+                        <h1>Welkom op de admin van Gasten in Zeeland.</h1>
+                        @include('common.notifications')
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium error
+                        inventore modi neque nulla porro dolorem provident, molestias vero ea
+                        earum asperiores optio et minus eius sint. Distinctio, ut deserunt.
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+</div>
 
 @endsection
